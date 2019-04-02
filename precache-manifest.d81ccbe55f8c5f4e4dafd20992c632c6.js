4bb3aaf6482f27b49fee60dd31cc1429f23fdaa1
AnsurM <ansurmehdi@gmail.com>
2019-04-03 01:42:20 +0500

    Updates

---
+++ precache-manifest.d81ccbe55f8c5f4e4dafd20992c632c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "109c83db77b4455c758e",
+    "url": "/AnsurM/fifaclientapi-frontend/static/js/runtime~main.f7142e13.js"
+  },
+  {
+    "revision": "ed2baf24005e671898fe",
+    "url": "/AnsurM/fifaclientapi-frontend/static/js/main.0cb90369.chunk.js"
+  },
+  {
+    "revision": "fc05dbae491881867900",
+    "url": "/AnsurM/fifaclientapi-frontend/static/js/2.0827f423.chunk.js"
+  },
+  {
+    "revision": "ed2baf24005e671898fe",
+    "url": "/AnsurM/fifaclientapi-frontend/static/css/main.82167697.chunk.css"
+  },
+  {
+    "revision": "fc05dbae491881867900",
+    "url": "/AnsurM/fifaclientapi-frontend/static/css/2.ce697ac3.chunk.css"
+  },
+  {
+    "revision": "c83c04649d9fa94135a0e6b44b4a378d",
+    "url": "/AnsurM/fifaclientapi-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AnsurM/precache-manifest.e0b9cb4b6fd85511d56c9e5fc5ea897a.js"
+  "/AnsurM/fifaclientapi-frontend/precache-manifest.d81ccbe55f8c5f4e4dafd20992c632c6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/AnsurM/index.html", {
+workbox.routing.registerNavigationRoute("/AnsurM/fifaclientapi-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
