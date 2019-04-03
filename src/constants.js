b863c910ca9008fe1d26e5ed926e1de3495bdda6
AnsurM <ansurmehdi@gmail.com>
2019-04-04 02:44:22 +0500

    trying to fix 401 error heroku

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 //export default {url:'http://192.168.0.103:3001'};
 
 export default {url: "https://cors-anywhere.herokuapp.com/https://git.heroku.com/serene-inlet-50937.git"};
+//export default {url: "https://git.heroku.com/serene-inlet-50937.git"};
